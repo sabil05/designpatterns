4de3a389b89af08f446bc34b2c556a7def0d2bf9
zhjuve <zhjuve.the.bower@gmail.com>
2019-12-11 15:05:05 +0300

    Update runbook.ps1

--- 000-vmrebuild/runbook.ps1
+++ 000-vmrebuild/runbook.ps1
@@ -1,1 +1,18 @@
-Some script
+# Ensures you do not inherit an AzureRMContext in your runbook
+Disable-AzureRmContextAutosave –Scope Process
+
+$connection = Get-AutomationConnection -Name AzureRunAsConnection
+while(!($connectionResult) -And ($logonAttempt -le 10))
+{
+    $LogonAttempt++
+    # Logging in to Azure...
+    $connectionResult =    Connect-AzureRmAccount `
+                               -ServicePrincipal `
+                               -Tenant $connection.TenantID `
+                               -ApplicationID $connection.ApplicationID `
+                               -CertificateThumbprint $connection.CertificateThumbprint
+
+    Start-Sleep -Seconds 30
+}
+
+Stop-AzureRmVM -Name 'RebuildableVM01' -ResourceGroupName 'ResourceGroupName'
