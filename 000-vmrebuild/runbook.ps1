e0a45df4f5fca88de57c6fef12301a85e4c23059
zhjuve <zhjuve.the.bower@gmail.com>
2019-12-11 18:23:28 +0300

    Update runbook.ps1

--- 000-vmrebuild/runbook.ps1
+++ 000-vmrebuild/runbook.ps1
@@ -1,25 +1,159 @@
-$connectionName = "AzureRunAsConnection"
-try
-{
-    # Get the connection "AzureRunAsConnection "
-    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+[OutputType("PSAzureOperationResponse")]
+param
+(
+    [Parameter (Mandatory=$false)]
+    [object] $WebhookData
+)
+$ErrorActionPreference = "stop"
 
-    "Logging in to Azure..."
-    Add-AzureRmAccount `
-        -ServicePrincipal `
-        -TenantId $servicePrincipalConnection.TenantId `
-        -ApplicationId $servicePrincipalConnection.ApplicationId `
-        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
-}
-catch {
-    if (!$servicePrincipalConnection)
+if ($WebhookData)
+{
+    # Get the data object from WebhookData
+    $WebhookBody = (ConvertFrom-Json -InputObject $WebhookData.RequestBody)
+
+    # Get the info needed to identify the VM (depends on the payload schema)
+    $schemaId = $WebhookBody.schemaId
+    Write-Verbose "schemaId: $schemaId" -Verbose
+    if ($schemaId -eq "azureMonitorCommonAlertSchema") {
+        # This is the common Metric Alert schema (released March 2019)
+        $Essentials = [object] ($WebhookBody.data).essentials
+        $AlertContext = [object] ($WebhookBody.data).alertContext
+        # Get the first target only as this script doesn't handle multiple
+        $alertTargetIdArray = (($Essentials.alertTargetIds)[0]).Split("/")
+        $SubId = ($alertTargetIdArray)[2]
+        $ResourceGroupName = ($alertTargetIdArray)[4]
+        $ResourceType = ($alertTargetIdArray)[6] + "/" + ($alertTargetIdArray)[7]
+        $ResourceName = ($alertTargetIdArray)[-1]
+        $status = $Essentials.monitorCondition
+        $alertCIs = (($AlertContext.AffectedConfigurationItems)[0]).Split("/")
+        Write-Verbose "Configuration Item: $alertCIs" -Verbose
+    }
+    elseif ($schemaId -eq "AzureMonitorMetricAlert") {
+        # This is the near-real-time Metric Alert schema
+        $AlertContext = [object] ($WebhookBody.data).context
+        $SubId = $AlertContext.subscriptionId
+        $ResourceGroupName = $AlertContext.resourceGroupName
+        $ResourceType = $AlertContext.resourceType
+        $ResourceName = $AlertContext.resourceName
+        $status = ($WebhookBody.data).status
+    }
+    elseif ($schemaId -eq "Microsoft.Insights/activityLogs") {
+        # This is the Activity Log Alert schema
+        $AlertContext = [object] (($WebhookBody.data).context).activityLog
+        $SubId = $AlertContext.subscriptionId
+        $ResourceGroupName = $AlertContext.resourceGroupName
+        $ResourceType = $AlertContext.resourceType
+        $ResourceName = (($AlertContext.resourceId).Split("/"))[-1]
+        $status = ($WebhookBody.data).status
+    }
+    elseif ($schemaId -eq $null) {
+        # This is the original Metric Alert schema
+        $AlertContext = [object] $WebhookBody.context
+        $SubId = $AlertContext.subscriptionId
+        $ResourceGroupName = $AlertContext.resourceGroupName
+        $ResourceType = $AlertContext.resourceType
+        $ResourceName = $AlertContext.resourceName
+        $status = $WebhookBody.status
+    }
+    else {
+        # Schema not supported
+        Write-Error "The alert data schema - $schemaId - is not supported."
+    }
+
+    Write-Verbose "status: $status" -Verbose
+    if (($status -eq "Activated") -or ($status -eq "Fired"))
     {
-        $ErrorMessage = "Connection $connectionName not found."
-        throw $ErrorMessage
-    } else{
-        Write-Error -Message $_.Exception
-        throw $_.Exception
+        Write-Verbose "resourceType: $ResourceType" -Verbose
+        Write-Verbose "resourceName: $ResourceName" -Verbose
+        Write-Verbose "resourceGroupName: $ResourceGroupName" -Verbose
+        Write-Verbose "subscriptionId: $SubId" -Verbose
+
+        # Determine code path depending on the resourceType
+        if ($ResourceType -eq "microsoft.operationalinsights/workspaces")
+        {
+            Write-Verbose "This is log search query-based alert parsing..." -Verbose
+
+            # Authenticate to Azure with service principal and certificate and set subscription
+            Write-Verbose "Authenticating to Azure with service principal and certificate" -Verbose
+            $ConnectionAssetName = "AzureRunAsConnection"
+            Write-Verbose "Get connection asset: $ConnectionAssetName" -Verbose
+            $Conn = Get-AutomationConnection -Name $ConnectionAssetName
+            if ($Conn -eq $null)
+            {
+                throw "Could not retrieve connection asset: $ConnectionAssetName. Check that this asset exists in the Automation account."
+            }
+            Write-Verbose "Authenticating to Azure with service principal." -Verbose
+            Add-AzureRMAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint | Write-Verbose
+            Write-Verbose "Setting subscription to work against: $SubId" -Verbose
+            Set-AzureRmContext -SubscriptionId $SubId -ErrorAction Stop | Write-Verbose
+
+            # Find out VM name from Affected Configuration Items array
+            Write-Verbose "Stopping the VM - $ResourceName - in resource group - $ResourceGroupName" -Verbose
+            #Stop-AzureRmVM -Name $ResourceName -ResourceGroupName $ResourceGroupName -Force
+            #Start-AzureRmVM -Name $ResourceName -ResourceGroupName $ResourceGroupName
+            $vm = Get-AzureRmVm -Name $alertCIs -ResourceGroupName $ResourceGroupName
+            Write-Verbose "Getting VM details... for $alertCIs" -Verbose
+            $vm
+
+        }
+        elseif ($ResourceType -eq "Microsoft.Compute/virtualMachines")
+        {
+            # This is an Resource Manager VM
+            Write-Verbose "This is an Resource Manager VM." -Verbose
+
+            # Authenticate to Azure with service principal and certificate and set subscription
+            Write-Verbose "Authenticating to Azure with service principal and certificate" -Verbose
+            $ConnectionAssetName = "AzureRunAsConnection"
+            Write-Verbose "Get connection asset: $ConnectionAssetName" -Verbose
+            $Conn = Get-AutomationConnection -Name $ConnectionAssetName
+            if ($Conn -eq $null)
+            {
+                throw "Could not retrieve connection asset: $ConnectionAssetName. Check that this asset exists in the Automation account."
+            }
+            Write-Verbose "Authenticating to Azure with service principal." -Verbose
+            Add-AzureRMAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint | Write-Verbose
+            Write-Verbose "Setting subscription to work against: $SubId" -Verbose
+            Set-AzureRmContext -SubscriptionId $SubId -ErrorAction Stop | Write-Verbose
+
+            # Stop the Resource Manager VM
+            Write-Verbose "Stopping the VM - $ResourceName - in resource group - $ResourceGroupName -" -Verbose
+            #Stop-AzureRmVM -Name $ResourceName -ResourceGroupName $ResourceGroupName -Force
+            #Start-AzureRmVM -Name $ResourceName -ResourceGroupName $ResourceGroupName
+            $vm = Get-AzureRmVm -Name $ResourceName -ResourceGroupName $ResourceGroupName
+            Write-Verbose "Getting VM details... for $ResourceName" -Verbose
+            $vm
+            # get 
+            $diagSa = [regex]::match($vm.DiagnosticsProfile.bootDiagnostics.storageUri, '^http[s]?://(.+?)\.').groups[1].value
+            Write-Verbose "Diagnostic storage container for $ResourceName :" -Verbose
+            Write-Verbose "$diagSa" -Verbose
+            # get VM parameters
+            $azureLocation              = $vm.Location
+            $azureResourceGroup         = $vm.ResourceGroupName
+            $azureVmName                = $vm.Name
+            $azureVmOsDiskName          = $vm.StorageProfile.OsDisk.Name
+            $azureVmSize                = $vm.HardwareProfile.VmSize
+            # get nic
+            foreach($nicUri in $vm.NetworkProfile.NetworkInterfaces.Id) {
+            $nic = Get-AzNetworkInterface -ResourceGroupName $vm.ResourceGroupName -Name $nicUri.Split('/')[-1]
+            Remove-AzNetworkInterface -Name $nic.Name -ResourceGroupName $vm.ResourceGroupName -Force
+            foreach($ipConfig in $nic.IpConfigurations) {
+                if($ipConfig.PublicIpAddress -ne $null) {
+                    Remove-AzPublicIpAddress -ResourceGroupName $vm.ResourceGroupName -Name $ipConfig.PublicIpAddress.Id.Split('/')[-1] -Force
+                }
+             }
+            }
+        }
+        else {
+            # ResourceType not supported
+            Write-Error "$ResourceType is not a supported resource type for this runbook."
+        }
+    }
+    else {
+        # The alert status was not 'Activated' or 'Fired' so no action taken
+        Write-Verbose ("No action taken. Alert status: " + $status) -Verbose
     }
 }
-
-Stop-AzureRmVM -Name 'RebuildableVM01' -ResourceGroupName 'RG-WE-RebuildableVMs' -Force;
+else {
+    # Error
+    Write-Error "This runbook is meant to be started from an Azure alert webhook only."
+}
