180be9e2ec1942ac69e0785081673bdff273640b
zhjuve <zhjuve.the.bower@gmail.com>
2019-12-18 14:21:28 +0300

    Update runbook.ps1

--- 000-vmrebuild/runbook.ps1
+++ 000-vmrebuild/runbook.ps1
@@ -25,7 +25,8 @@ if ($WebhookData)
         $ResourceType = ($alertTargetIdArray)[6] + "/" + ($alertTargetIdArray)[7]
         $ResourceName = ($alertTargetIdArray)[-1]
         $status = $Essentials.monitorCondition
-        $alertCI = (($Essentials.alertRule).Split("-"))[-1]
+        $alertRule = $Essentials.alertRule
+        $alertCI = (($alertRule).Split("-"))[-1]
         #Write-Verbose "Configuration Item: $alertCIs" -Verbose
         Write-Verbose "Configuration Item: $alertCI" -Verbose
         $alertCI
@@ -93,11 +94,37 @@ if ($WebhookData)
             #Stop-AzureRmVM -Name $ResourceName -ResourceGroupName $ResourceGroupName -Force
             #Start-AzureRmVM -Name $ResourceName -ResourceGroupName $ResourceGroupName
             if (!($alertCI -eq $null)) {
+                #disable alert rule to avoid false positives
+                Write-Verbose "Disanling Alert Rule = $alertRule" -Verbose
+				$SubscriptionId = $context.Subscription
+				$cache = $context.TokenCache
+				$cacheItem = $cache.ReadItems()
+				$AccessToken=$cacheItem[$cacheItem.Count -1].AccessToken
+				$headerParams = @{'Authorization'="Bearer $AccessToken"}
+				$url="https://management.azure.com/subscriptions/$SubscriptionId/resourceGroups/$ResourceGroupName/providers/microsoft.insights/scheduledQueryRules/AR-HeartbeatsLost-RebuildableVM01?api-version=2018-04-16"
+				$rBody = @{
+					'properties' = @{
+						'enabled' = 'false'
+					}
+				}
+				$json = $rBody | ConvertTo-Json
+				$results=Invoke-RestMethod -Uri $url -Headers $headerParams -Method Patch -Body $json -ContentType 'application/json'
+				$results
+                #get old VM config
                 Write-Verbose "Getting the VM = $alertCI" -Verbose
                 $oldvm = Get-AzureRmVm -Name $alertCI -ResourceGroupName $ResourceGroupName
                 $oldvm
                 Write-Verbose "Deleting old VM ($oldvm.Name)" -Verbose
                 Remove-AzureRmVM -Name $oldvm.Name -ResourceGroupName $ResourceGroupName -Force
+                $osDisk = Get-AzureRmDisk -DiskName $oldvm.StorageProfile.OsDisk.Name -ResourceGroupName $oldvm.ResourceGroupName
+                $osDisk
+                $vmConfig = New-AzureRmVMConfig -VMName $oldvm.Name -VMSize $oldvm.HardwareProfile.VmSize
+                $vmConfig
+                $vm = Add-AzureRmVMNetworkInterface -VM $vmConfig -Id $oldvm.NetworkProfile.NetworkInterfaces.Id
+                $vm
+                $vm = Set-AzureRmVMOSDisk -VM $vm -ManagedDiskId $osDisk.Id -CreateOption Attach -Linux
+                $vm
+                New-AzureRmVM -VM $vm -ResourceGroupName $oldvm.ResourceGroupName -Location $oldvm.Location
             } elseif (!((Get-AzureRmVm -Name "RebuildableVM01" -ResourceGroupName "RG-WE-RebuildableVMs") -eq $null )) {
                 # test use only
                 Write-Verbose "Taking default VM = RebuidableVM01test" -Verbose
@@ -113,15 +140,7 @@ if ($WebhookData)
             #$azureVmName                = $vm.Name
             #$azureVmOsDiskName          = $vm.StorageProfile.OsDisk.Name
             #$azureVmSize                = $vm.HardwareProfile.VmSize
-            $osDisk = Get-AzureRmDisk -DiskName $oldvm.StorageProfile.OsDisk.Name -ResourceGroupName $oldvm.ResourceGroupName
-            $osDisk
-            $vmConfig = New-AzureRmVMConfig -VMName $oldvm.Name -VMSize $oldvm.HardwareProfile.VmSize
-            $vmConfig
-            $vm = Add-AzureRmVMNetworkInterface -VM $vmConfig -Id $oldvm.NetworkProfile.NetworkInterfaces.Id
-            $vm
-            $vm = Set-AzureRmVMOSDisk -VM $vm -ManagedDiskId $osDisk.Id -CreateOption Attach
-            $vm
-            New-AzureRmVM -VM $vm -ResourceGroupName $oldvm.ResourceGroupName -Location $oldvm.Location
+
         }
         elseif ($ResourceType -eq "Microsoft.Compute/virtualMachines")
         {
